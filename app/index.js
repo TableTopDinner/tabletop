46d9ef92e84d5082fc240a4da23edff5519d5fc8
Thomas Cho <s.thomas.cho@gmail.com>
2014-09-27 15:26:51 -0700

    Add gzippo express

--- app/index.js
+++ app/index.js
@@ -1,14 +1,7 @@
-var express = require('express')
+var gzippo = require('gzippo');
+var express = require('express');
 var app = express();
-
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname))
-
-app.get('/', function(request, response) {
-  response.send('Hello TableTop!')
-})
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
-
+ 
+app.use(express.logger('dev'));
+app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.listen(process.env.PORT || 5000);
