f892ec82a63d343fa9f1ad186df836b0dd282831
Thomas Cho <s.thomas.cho@gmail.com>
2014-11-10 23:25:24 -0700

    Initial FB reference call & Restaurant Data Structure

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -9,10 +9,39 @@
  */
 angular.module('tabletopApp')
   .controller('DashboardCtrl', function ($scope) {
+    
     $scope.alert = function(){
-	alert("HEELO");
-	};
-});
+		alert("HEELO");
+		};
+
+		//PRODUCTION DB:'https://tabletopdinner.firebaseio.com/' ||| STAGING DB:'https://tabletopstaging.firebaseio.com/'
+    var tableRef = new Firebase('https:////tabletopstaging.firebaseio.com/'); 
+    
+    //Instantiating a new array for restaurants 
+    $scope.restaurants = []; 
+
+		//Taking a `snapshot` or `image` of our database stored in the `tableRef` reference to use in our app
+    var j = 0;
+    tableRef.once('value', function(allSnapshot) {
+    		//For each restaurant in our DB
+        allSnapshot.forEach(function(restaurantSnapshot) {
+            var i = restaurantSnapshot.child('id').val();
+            if( i !== null ){
+              $scope.restaurants[j] = campaignSnapshot.val();
+            }
+
+            $scope.$apply($scope.restaurants);
+            console.log("Restaurant: " + j);    
+       });
+        console.log("Restaurants All Loaded");   
+    });
+
+    //Created selectedRestaurant with data retrieved from database to be used within the views.
+    $scope.selectRestaurant = function(object) {
+        $scope.selectedRestaurant = object;
+    };
+
+}); //This is the end of the page dum dum dum (angular.module) ("'\(>.<)/") <-- zach 
 
 
 
