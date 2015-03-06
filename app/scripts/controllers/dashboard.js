3447ad55cfc74a040a0511d54e5085b074384538
Thomas Cho <s.thomas.cho@gmail.com>
2015-03-06 07:24:18 -0700

    Locations and Zapier connected

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -35,23 +35,29 @@ angular.module('tabletopApp')
     $scope.eventsShow = false;
     $scope.analysisShow = false;
     $scope.paymentsShow = false;
+    $scope.locationsShow = false;
     $scope.dashboardTemplateLoader = function(template) {
          if(template == 'main') {
-             $scope.mainShow = true; $scope.restaurantsShow = false; $scope.eventsShow = false; $scope.analysisShow = false; $scope.paymentsShow = false;
+             $scope.mainShow = true; $scope.restaurantsShow = false; $scope.eventsShow = false; $scope.analysisShow = false; $scope.paymentsShow = false; $scope.locationsShow = false;
          }
-         if(template == 'restaurants') {
-             $scope.mainShow = false; $scope.restaurantsShow = true; $scope.eventsShow = false; $scope.analysisShow = false; $scope.paymentsShow = false;
+         else if(template == 'restaurants') {
+             $scope.mainShow = false; $scope.restaurantsShow = true; $scope.eventsShow = false; $scope.analysisShow = false; $scope.paymentsShow = false; $scope.locationsShow = false;
          }
-         if(template == 'events') {
-             $scope.mainShow = false; $scope.restaurantsShow = false; $scope.eventsShow = true; $scope.analysisShow = false; $scope.paymentsShow = false;
+         else if(template == 'events') {
+             $scope.mainShow = false; $scope.restaurantsShow = false; $scope.eventsShow = true; $scope.analysisShow = false; $scope.paymentsShow = false; $scope.locationsShow = false;
          }
-         if(template == 'analysis') {
-             $scope.mainShow = false; $scope.restaurantsShow = false; $scope.eventsShow = false; $scope.analysisShow = true; $scope.paymentsShow = false;
+         else if(template == 'analysis') {
+             $scope.mainShow = false; $scope.restaurantsShow = false; $scope.eventsShow = false; $scope.analysisShow = true; $scope.paymentsShow = false; $scope.locationsShow = false;
          }
-         if(template == 'payments') {
-             $scope.mainShow = false; $scope.restaurantsShow = false; $scope.eventsShow = false; $scope.analysisShow = false; $scope.paymentsShow = true;
+         else if(template == 'payments') {
+             $scope.mainShow = false; $scope.restaurantsShow = false; $scope.eventsShow = false; $scope.analysisShow = false; $scope.paymentsShow = true; $scope.locationsShow = false;
+         }
+         else if(template == 'locations') {
+             $scope.mainShow = false; $scope.restaurantsShow = false; $scope.eventsShow = false; $scope.analysisShow = false; $scope.paymentsShow = false; $scope.locationsShow = true;
          }
     }
+
+    $scope.map = { center: {latitude: 33.636453, longitude: -112.410736}, zoom: 10 };
 
     //Load our current user, in order to create restaurants for this user
     $scope.user = user;
@@ -67,48 +73,78 @@ angular.module('tabletopApp')
 		//PRODUCTION DB:'https://tabletopdinner.firebaseio.com/' ||| STAGING DB:'https://tabletopstaging.firebaseio.com/'
     // var tableRef = new Firebase('https://tabletopstaging.firebaseio.com/');
     var restaurantsRef = new Firebase('https://tabletopstaging.firebaseio.com/restaurants/');  
-    
     var callRestaurantRef = $firebase(restaurantsRef); //<<< Constructor for firebase
+
+    var eventsRef = new Firebase('https://tabletopstaging.firebaseio.com/events/');  
+    var callEventRef = $firebase(eventsRef); //<<< Constructor for firebase
 
     // console.log(callRestaurantRef);
 
     //Instantiating a new array for restaurants 
     $scope.restaurants = []; 
+    //Instantiating a new array for restaurants 
+    $scope.events = []; 
+
     //To identify when all restaurants are loaded to show them all in sync
     $scope.restaurantsLoaded = false;
+    $scope.eventsLoaded = false;
 
 		//Taking a `snapshot` or `image` of our database stored in the `tableRef` reference to use in our app
-    var j = 0;
     restaurantsRef.once('value', function(allSnapshot) {
     		//For each restaurant in our DB
-        console.log(allSnapshot);
         allSnapshot.forEach(function(restaurantSnapshot) {
             var i = restaurantSnapshot.child('id').val();
-            if( i !== null ){
+            var u = restaurantSnapshot.child('userID').val(); //var to create array of only the values with correct current user's id 
+
+            if( i !== null && u === user.id){
             	// set database content into our restaurants array
-              $scope.restaurants[j] = restaurantSnapshot.val();
+              $scope.restaurants[i] = restaurantSnapshot.val();
             }
 
-            $scope.$apply($scope.allSnapshot);
-            console.log("Restaurant: " + j);
-            console.log("Restaurant: " + $scope.restaurants);    
+            $scope.$apply($scope.restaurants);
+            console.log("Restaurant: " + i);
        });
-      console.log("Restaurants All Loaded");   
+      console.log("All restaurants loaded after this call");   
       $scope.restaurantsLoaded = true;
+      console.log('Array', $scope.restaurants);
     });
 
-    $scope.data = restaurantsRef.$asObject();
 
-    console.log('Array', $scope.restaurants);
-    // console.log('Object', $scope.data);
+    eventsRef.once('value', function(allSnapshot) {
+        //For each restaurant in our DB
+        allSnapshot.forEach(function(eventSnapshot) {
+            var i = eventSnapshot.child('id').val();
+            // var r = eventSnapshot.child('rID').val();
+
+            if( i !== null ){
+              // set database content into our events array
+              $scope.events[i] = eventSnapshot.val();
+            }
+
+            $scope.$apply($scope.events);
+            console.log("Event: " + i);
+       });
+      console.log("All events loaded after this call");   
+      $scope.eventsLoaded = true;
+      console.log('Array', $scope.events);
+    });
 
 
     //Refreshes the add restaurant modal everytime it is selected (clicked on).
-    $scope.instantiateRestaurant = function(object) {
+    $scope.instantiateRestaurant = function() {
     	$scope.selectedRestaurant = {};
       // $scope.selectedRestaurant.name = ""; Name can not be empty
       $scope.selectedRestaurant.description = "";
-      $scope.selectedRestaurant.type = "";
+      $scope.selectedRestaurant.type = ""; //Types: BBQ, Steakhouse, Bar and Grill, Italian, Wine Bar, Mexican
+      $scope.selectedRestaurant.website = "";
+      $scope.selectedRestaurant.address1 = "";
+      $scope.selectedRestaurant.address2 = "";
+      $scope.selectedRestaurant.city = "";
+      $scope.selectedRestaurant.state = "";
+      $scope.selectedRestaurant.zipcode = "";
+      $scope.selectedRestaurant.email = "";
+      $scope.selectedRestaurant.phoneNum = "";
+      $scope.selectedRestaurant.userID = user.id;
       
       //Removes alerts if there previously
       document.getElementById("missingFieldError").innerHTML = "";
@@ -121,6 +157,7 @@ angular.module('tabletopApp')
 
     //Called when 'Create' button is pressed within the Create New Restaurant modal
     $scope.createRestaurant = function() {
+
     	//The new restaurant about to be created will always be added to the end of the array
     	var creatingID = $scope.restaurants.length;
 
@@ -129,24 +166,25 @@ angular.module('tabletopApp')
           // (Need It For Image Upload Ignore For Now) document.getElementById("missingFieldError").innerHTML = "<div class='alert alert-info'> <strong>Loading Restuarant...</strong>";
 
           var dataObject;
-          if(result === true) {
-              dataObject = {
-                  "name" : $scope.selectedRestaurant.name, "description": $scope.selectedRestaurant.description, "type": $scope.selectedRestaurant.type, "imgSrc": resizedImgSrcData, "thumbSrc": resizedThumbSrcData, "brand": $scope.selectedRestaurant.brand, "region": $scope.selectedRestaurant.region, "location": $scope.selectedRestaurant.location, "contact": $scope.selectedRestaurant.contact, "url": $scope.selectedRestaurant.url, "status": $scope.selectedRestaurant.status, "startDate": $scope.selectedRestaurant.startDate, "endDate": $scope.selectedRestaurant.endDate, "canvas": $scope.selectedRestaurant.canvas, "hashtags": $scope.selectedRestaurant.hashtags, "publish": true, "id": addID, "brandImgSrc": resizedBrandImgSrcData, "brandThumbSrc": resizedBrandThumbSrcData 
-              };
-          } else {
-              dataObject = {
-                  "name" : $scope.selectedRestaurant.name, "description": $scope.selectedRestaurant.description, "type": $scope.selectedRestaurant.type, "imgSrc": resizedImgSrcData, "thumbSrc": resizedThumbSrcData, "brand": $scope.selectedRestaurant.brand, "region": $scope.selectedRestaurant.region, "location": $scope.selectedRestaurant.location, "contact": $scope.selectedRestaurant.contact, "url": $scope.selectedRestaurant.url, "status": $scope.selectedRestaurant.status, "startDate": $scope.selectedRestaurant.startDate, "endDate": $scope.selectedRestaurant.endDate, "canvas": $scope.selectedRestaurant.canvas, "hashtags": $scope.selectedRestaurant.hashtags, "publish": false, "id": addID, "brandImgSrc": resizedBrandImgSrcData, "brandThumbSrc": resizedBrandThumbSrcData
-              };
-          }
+          // if(result === true) {
+          dataObject = {
+              "id": creatingID, "name" : $scope.selectedRestaurant.name, "description": $scope.selectedRestaurant.description, "type": $scope.selectedRestaurant.type, "website": $scope.selectedRestaurant.website, "address1": $scope.selectedRestaurant.address1, "address2": $scope.selectedRestaurant.address2, "city": $scope.selectedRestaurant.city, "state": $scope.selectedRestaurant.state, "zipcode": $scope.selectedRestaurant.zipcode, "email": $scope.selectedRestaurant.email, "phoneNum": $scope.selectedRestaurant.phoneNum, "userID": $scope.selectedRestaurant.userID
+          };
+          // } else {
+          //     dataObject = {
+          //         "name" : $scope.selectedRestaurant.name, "description": $scope.selectedRestaurant.description, "address": $scope.selectedRestaurant.address, "email": $scope.selectedRestaurant.email, "phoneNum": $scope.selectedRestaurant.phoneNum, "license": $scope.selectedRestaurant.license
+          //     };
+          // }
 
-          fireRef.child(creatingID).set(dataObject);
+          restaurantsRef.child(creatingID).set(dataObject);
+          location.reload();
           //wait 3000 mili secs as default to give time to load image. 
           // (Need It For Image Upload Ignore For Now) $timeout( function() {spinner.stop(); $scope.restuarant.push(dataObject); document.getElementById("missingFieldError").innerHTML = "<div class='alert alert-success'> <strong>Success!</strong>";}, 3000, true);
       }
       //If there are missing field errors alert that silly reatuarnt owner
       catch(err) {
           $('#missingFieldError').show();
-          spinner.stop();
+          alert(err);
           if ( err.message === "Cannot read property 'name' of undefined") {
               document.getElementById("missingFieldError").innerHTML = "<div class='alert alert-danger'> <strong>Hey you mighty reatuarnt owner!</strong> Please give your restaurant a name.";
           } 
@@ -158,7 +196,67 @@ angular.module('tabletopApp')
 
 
 
-}); //This is the end of the page dum dum dum (angular.module) ("'\(>.<)/") <-- zach 
+    $scope.selectEvent = function(object) {
+       $scope.selectedEvent = object;
+    };
+
+    //Refreshes the add restaurant modal everytime it is selected (clicked on).
+    $scope.instantiateEvent = function() {
+      $scope.selectedEvent = {};
+      // $scope.selectedRestaurant.name = ""; Name can not be empty
+      $scope.selectedEvent.description = "";
+      $scope.selectedEvent.restaurant = "";
+      $scope.selectedEvent.price = "0";
+      $scope.selectedEvent.image = "https://media.licdn.com/media/p/5/000/283/112/1959ca5.png";
+      // $scope.selectedEvent.rID = ;
+      
+      //Removes alerts if there previously
+      document.getElementById("missingFieldError").innerHTML = "";
+    }
+
+    $scope.createEvent = function() {
+
+      //The new restaurant about to be created will always be added to the end of the array
+      var creatingID = $scope.events.length;
+
+      //If no missing field errors continue
+      try {
+          // (Need It For Image Upload Ignore For Now) document.getElementById("missingFieldError").innerHTML = "<div class='alert alert-info'> <strong>Loading Restuarant...</strong>";
+          
+
+
+          var dataObject;
+          // if(result === true) {
+          dataObject = {
+              "id": creatingID, "name" : $scope.selectedEvent.name, "description": $scope.selectedEvent.description, "restaurant": $scope.selectedEvent.restaurant, "price": $scope.selectedEvent.price, "image": $scope.selectedEvent.image
+          };
+          // } else {
+          //     dataObject = {
+          //         "name" : $scope.selectedRestaurant.name, "description": $scope.selectedRestaurant.description, "address": $scope.selectedRestaurant.address, "email": $scope.selectedRestaurant.email, "phoneNum": $scope.selectedRestaurant.phoneNum, "license": $scope.selectedRestaurant.license
+          //     };
+          // }
+
+          eventsRef.child(creatingID).set(dataObject);
+          console.log("Success?");
+          location.reload();
+          //wait 3000 mili secs as default to give time to load image. 
+          // (Need It For Image Upload Ignore For Now) $timeout( function() {spinner.stop(); $scope.restuarant.push(dataObject); document.getElementById("missingFieldError").innerHTML = "<div class='alert alert-success'> <strong>Success!</strong>";}, 3000, true);
+      }
+      //If there are missing field errors alert that silly reatuarnt owner
+      catch(err) {
+          $('#missingFieldError').show();
+          alert(err);
+          if ( err.message === "Cannot read property 'name' of undefined") {
+              document.getElementById("missingFieldError").innerHTML = "<div class='alert alert-danger'> <strong>Hey you mighty reatuarnt owner!</strong> Please give your event a name.";
+          } 
+          if ( err.message === "Firebase.set failed: First argument contains undefined in property 'name'") {
+              document.getElementById("missingFieldError").innerHTML = "<div class='alert alert-danger'> <strong>Hey you mighty reatuarnt owner!</strong> Please give your event a name.";
+          }
+      }
+    }
+
+
+}); //This is the end of the page dum dum dum (angular.module) ("'\(>.<)/") <-- "that is zach" 
 
 
 
---
+++ app/scripts/directives/googleMaps.js
@@ -0,0 +1,46 @@
+/**
+ * Created by csiebler on 2/19/15.
+ */
+/**
+ * @ngdoc function
+ * @name tabletopApp.directive:googleMap
+ * @description
+ * # googleMapDirective
+ * Displays a Map using the Google Map API
+ */
+angular.module('tabletopApp')
+  .directive('googleMap', function () {
+    return {
+      restrict: 'E',
+      replace: true,
+      template: '<div></div>',
+      link: function($scope, element, attrs) {
+        var center = new google.maps.LatLng(33.448377, -112.074037);
+
+        var map_options = {
+          zoom: 12,
+          center: center,
+          mapTypeId: google.maps.MapTypeId.SATELLITE
+        };
+
+        // create map
+        var map = new google.maps.Map(document.getElementById(attrs.id), map_options);
+
+        // configure marker
+        var marker_options = {
+          map: map,
+          position: center
+        };
+
+        // create marker
+        var marker = new google.maps.Marker(marker_options);
+
+        $scope.$watch('locationsShow', function () {
+          window.setTimeout(function(){
+            google.maps.event.trigger(map, 'resize');
+            map.setCenter(center);
+          },100);
+        });
+      }
+    }
+  });
